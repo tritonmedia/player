678e1e6f4caacdf01a502d402ad91f2060dad6b7
Jared Allard <jaredallard@outlook.com>
2019-08-20 10:50:30 -0700

    chore: sync testing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import LolmoRow from './components/LolmoRow'
 
 function App() {
-  const items = [
+  const anime = [
     {
       id: 0,
       title: "KonoSuba",
@@ -17,17 +17,56 @@ function App() {
     },
     {
       id: 3,
-      title: "KonoSuba",
-      imageUrl: "https://image.tmdb.org/t/p/original/5Tq8v0HhzUX1QJWcmef5HyhKCkh.jpg",
+      title: "Sword Art Online",
+      imageUrl: "https://www.thetvdb.com/banners/fanart/original/259640-17.jpg",
     },
   ]
+
+  const movies = [
+    {
+      id: 0,
+      title: "Your Name",
+      imageUrl: "https://image.tmdb.org/t/p/w500_and_h282_face/7OMAfDJikBxItZBIug0NJig5DHD.jpg"
+    },
+    {
+      id: 1,
+      title: 'A Slient Voice',
+      imageUrl: "https://image.tmdb.org/t/p/w500_and_h282_face/5lAMQMWpXMsirvtLLvW7cJgEPkU.jpg"
+    },
+    {
+      id: 3,
+      title: "Spirited Away",
+      imageUrl: "https://image.tmdb.org/t/p/w500_and_h282_face/djgM2d3e42p9GFQObg6lwK2SVw2.jpg",
+    },
+  ]
+
+  const suggested = [
+    {
+      id: 0,
+      title: "Avatar: The Last Airbender",
+      imageUrl: "https://www.thetvdb.com/banners/fanart/original/74852-5.jpg"
+    },
+    {
+      id: 2,
+      title: "Assasination Classroom",
+      imageUrl: "https://www.thetvdb.com/banners/fanart/original/283947-1.jpg"
+    },
+    {
+      id: 3,
+      title: "The Devil is a Part Timer",
+      imageUrl: "https://www.thetvdb.com/banners/fanart/original/267441-3.jpg"
+    },
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="App-logo">TRITON</div>
       </header>
       <div className="App-main">
-        <LolmoRow name="Next Up" items={items}/>
+        <LolmoRow name="Anime" items={anime}/>
+        <LolmoRow name="Movies" items={movies}/>
+        <LolmoRow name="Based on your Recent History" items={suggested}/>
       </div>
     </div>
   );
